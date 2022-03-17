2b07309e748681e86fda701c1dadead196cb608f
lance <lance@codepy.net>
2022-03-17 19:03:09 +0800

    k8s crd static....

---
+++ controllers/app_resources.go
@@ -0,0 +1,149 @@
+package controllers
+
+import (
+	devopsv1 "k8s-crd-caddy/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/pointer"
+)
+
+func NewDeployment(app *devopsv1.Static) *appsv1.Deployment {
+	selector := map[string]string{"app": controllerName}
+	return &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      controllerName,
+			Namespace: app.Namespace,
+			Labels:    selector,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Strategy: appsv1.DeploymentStrategy{
+				Type: appsv1.RollingUpdateDeploymentStrategyType,
+			},
+			Replicas: pointer.Int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: selector,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      controllerName,
+					Namespace: app.Namespace,
+					Labels:    selector,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  controllerName,
+							Image: "lanceyuan/caddy:v2.4.6",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      corev1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+							LivenessProbe: &corev1.Probe{
+								Handler:             corev1.Handler{HTTPGet: &corev1.HTTPGetAction{Port: intstr.IntOrString{Type: intstr.Int, IntVal: 8080}, Path: "/health", Scheme: corev1.URISchemeHTTP}},
+								InitialDelaySeconds: 10,
+								TimeoutSeconds:      10,
+								PeriodSeconds:       10,
+							},
+							ReadinessProbe: &corev1.Probe{
+								Handler:             corev1.Handler{HTTPGet: &corev1.HTTPGetAction{Port: intstr.IntOrString{Type: intstr.Int, IntVal: 8080}, Path: "/health", Scheme: corev1.URISchemeHTTP}},
+								InitialDelaySeconds: 10,
+								TimeoutSeconds:      10,
+								PeriodSeconds:       10,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func NewService(app *devopsv1.Static) *corev1.Service {
+	selector := map[string]string{"app": controllerName}
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      controllerName,
+			Namespace: app.Namespace,
+			Labels:    selector,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: selector,
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       80,
+					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 8080},
+				},
+				{
+					Name:       "admin",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       2019,
+					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 2019},
+				},
+			},
+			Type: corev1.ServiceTypeClusterIP,
+		},
+	}
+}
+
+func NewIngress(app *devopsv1.Static) *networkingv1.Ingress {
+	ingObj := &networkingv1.Ingress{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Ingress",
+			APIVersion: "networking.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+			Labels: map[string]string{
+				"app": app.Name,
+			},
+		},
+		Spec: networkingv1.IngressSpec{
+			IngressClassName: app.Spec.IngressClassName,
+		},
+	}
+	pathType := networkingv1.PathTypeImplementationSpecific
+	for _, host := range app.Spec.Hosts {
+		ingObj.Spec.Rules = append(ingObj.Spec.Rules, networkingv1.IngressRule{
+			Host: host,
+			IngressRuleValue: networkingv1.IngressRuleValue{
+				HTTP: &networkingv1.HTTPIngressRuleValue{
+					Paths: []networkingv1.HTTPIngressPath{
+						{
+							Path:     app.Spec.Path,
+							PathType: &pathType,
+							Backend: networkingv1.IngressBackend{
+								ServiceName: controllerName,
+								ServicePort: intstr.IntOrString{Type: intstr.Int, IntVal: 80},
+								//Service: &networkingv1.IngressServiceBackend{
+								//	Name: controllerName,
+								//	Port: networkingv1.ServiceBackendPort{
+								//		Name:   "http",
+								//		Number: 80,
+								//	},
+								//},
+							},
+						},
+					},
+				},
+			},
+		})
+	}
+	return ingObj
+}
---
+++ controllers/static_controller.go
@@ -0,0 +1,162 @@
+/*
+Copyright 2022 Lance Yuan.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"reflect"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	devopsv1 "k8s-crd-caddy/api/v1"
+)
+
+const (
+	controllerName string = "caddy-controller"
+)
+
+var logger logr.Logger
+
+// StaticReconciler reconciles a Static object
+type StaticReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=devops.codepy.net,resources=statics,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=devops.codepy.net,resources=statics/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=devops.codepy.net,resources=statics/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Static object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+func (r *StaticReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger = log.FromContext(ctx)
+
+	instance := &devopsv1.Static{}
+	// TODO(user): your logic here
+	if err := r.Get(ctx, req.NamespacedName, instance); err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("get App not found!!!!!")
+			return ctrl.Result{}, nil
+		}
+		logger.Info("get App error!!!!!")
+		return reconcile.Result{}, err
+	}
+	deployment := &appsv1.Deployment{}
+	svc := &corev1.Service{}
+	reqNamespaceName := req.NamespacedName
+	reqNamespaceName.Name = controllerName
+	if err := r.Client.Get(ctx, reqNamespaceName, deployment); err != nil {
+		if !errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+		deployment = NewDeployment(instance)
+		if err := r.Client.Create(ctx, deployment); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+	if err := r.Client.Get(ctx, reqNamespaceName, svc); err != nil {
+		if !errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+		svc = NewService(instance)
+		if err := r.Client.Create(ctx, svc); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+	if !instance.ObjectMeta.DeletionTimestamp.IsZero() {
+		logger.Info("start delete ingress....")
+		ingress := &networkingv1.Ingress{}
+		if err := r.Client.Get(ctx, req.NamespacedName, ingress); err != nil {
+			if errors.IsNotFound(err) {
+				instance.ObjectMeta.Finalizers = []string{}
+				if err := r.Update(ctx, instance); err != nil {
+					return ctrl.Result{}, err
+				}
+				return reconcile.Result{}, nil
+			}
+			return ctrl.Result{}, err
+		} else {
+			if err := r.Client.Delete(ctx, ingress); err != nil {
+				return ctrl.Result{}, err
+			} else {
+				instance.ObjectMeta.Finalizers = []string{}
+				if err := r.Update(ctx, instance); err != nil {
+					return ctrl.Result{}, err
+				}
+			}
+		}
+		return reconcile.Result{}, nil
+	}
+	ingress := &networkingv1.Ingress{}
+	if err := r.Client.Get(ctx, req.NamespacedName, ingress); err != nil {
+		if !errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+		logger.Info("add caddy route !!!!!")
+		if err := AddCaddyRoute(instance); err != nil {
+			logger.Info("add caddy route error !!!!!")
+			return ctrl.Result{}, err
+		}
+		ingress = NewIngress(instance)
+		logger.Info("create ingress !!!!!")
+		if err := r.Client.Create(ctx, ingress); err != nil {
+			return ctrl.Result{}, err
+		} else {
+			logger.Info("update App finalizer !!!!!")
+			instance.ObjectMeta.Finalizers = append(instance.ObjectMeta.Finalizers, instance.Name)
+			if err := r.Client.Update(ctx, instance); err != nil {
+				logger.Info("update finalizer err !!!!!")
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{}, nil
+		}
+	} else {
+		logger.Info("get Ingress exist !!!!!")
+		newIngress := NewIngress(instance)
+		if !reflect.DeepEqual(newIngress.Spec, ingress.Spec) {
+			logger.Info("update Ingress !!!!!")
+			if err := r.Client.Update(ctx, newIngress); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *StaticReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&devopsv1.Static{}).
+		Complete(r)
+}
